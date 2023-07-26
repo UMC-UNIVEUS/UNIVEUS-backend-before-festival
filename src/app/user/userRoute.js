c1f4315167ce5a51fea7af2c7a81c87bd3c14d53
임채연 <dlacodus0407@gmail.com>
2023-07-27 00:33:45 +0900

    Add : 휴대폰 본인인증 API

--- src/app/user/userRoute.js
+++ src/app/user/userRoute.js
@@ -1,5 +1,6 @@
 import express from "express"
-import {sendAuthNumber, login, loginRedirect, signup, signupRedirect} from "./userController"
+import {sendAuthNumber, login, loginRedirect, 
+    signup, signupRedirect, verifyNumber} from "./userController"
 const userRouter = express.Router();
 
 userRouter.get('/login', login);
@@ -7,5 +8,6 @@ userRouter.get('/login/redirect', loginRedirect);
 userRouter.get('/signup', signup);
 userRouter.get('/signup/redirect', signupRedirect);
 userRouter.post('/send/number', sendAuthNumber);
+userRouter.post('/auth/number', verifyNumber);
 
 export default userRouter;
