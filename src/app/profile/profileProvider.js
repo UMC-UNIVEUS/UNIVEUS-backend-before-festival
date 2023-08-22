285ce6be844c54f478232ea22e01cfb12f4a65bc
ReadMyMemory <lame3925@gmail.com>
2023-08-22 18:28:25 +0900

    Update : 새로운 디자인에 맞게 프로필 조회 API response 수정

--- src/app/profile/profileProvider.js
+++ src/app/profile/profileProvider.js
@@ -1,10 +1,12 @@
 import pool from '../../../config/database';
 import {
+    selectPostEndDate,
     selectUserbyId,
     selectUserDefaultProfilebyId,
     selectUserIntroProfilebyId,
     selectUserMyUnivebyId,
-    selectUserParticipatebyId
+    selectUserParticipatebyId,
+    selectPostbyId
 } from './profileDao';
 import {baseResponse, response, errResponse} from "../../../config/response";
 import dayjs from 'dayjs';
@@ -24,6 +26,12 @@ export const userIdCheck = async (user_id) => {
     return userIdCheckResult;
 };
 
+export const postIdCheck = async(post_id) => {
+    const connection = await pool.getConnection(async (conn) => conn);
+    const postIdCheckResult = selectPostbyId(connection, post_id);
+    connection.release();
+    return postIdCheckResult;
+};
 
 // 프로필 화면에서 상단에 위치하는 유저 기본 정보 프로필 내용을 불러오는 API
 /* export const showUserDefaultProfile= async (user_id) => {
@@ -53,7 +61,7 @@ export const userIdCheck = async (user_id) => {
 export const showUserMyUnive = async (user_id) => {
     const connection = await pool.getConnection(async (conn) => conn);
     const showUserMyUniveResult = await selectUserMyUnivebyId(connection, user_id);
-    connection.release();
+
     if(showUserMyUniveResult[0][0]) {
         for (let i = 0; i < showUserMyUniveResult[0].length; i++) {
             //학번 변경
@@ -65,14 +73,24 @@ export const showUserMyUnive = async (user_id) => {
             //모임 시간 변경
             if(showUserMyUniveResult[0][i].meeting_date) {
                 const datevalue = dayjs(showUserMyUniveResult[0][i].meeting_date);
-                showUserMyUniveResult[0][i].meeting_date = datevalue.month()+ 1 + "월" + datevalue.date() + "일";
-                showUserMyUniveResult[0][i].meeting_time = datevalue.hour() + ":" + datevalue.minute();
+                showUserMyUniveResult[0][i].meeting_date = datevalue.month() + 1 + "월" + datevalue.date() + "일 " + datevalue.hour() + ":" + datevalue.minute();
+            }
 
-                //D-day 추가
-                const nowDate = dayjs();
-                const diffDate = datevalue.diff(nowDate);
+            const postInfo = await postIdCheck(showUserMyUniveResult[0][i].post_id);
+
+            // 모임 인원 계산
+            const current_people = postInfo[0][0].current_people;
+            const limit_people = postInfo[0][0].limit_people;
+            showUserMyUniveResult[0][i].people_status = current_people + "/" + limit_people;
+
+            //D-day 계산
+            if(postInfo[0][0].end_date) {
+                const nowDate = dayjs(); // 현재 시간
+                const endDate = dayjs(postInfo[0][0].end_date); // 마감 시간
+                const diffDate = endDate.diff(nowDate); // 마감 시간 - 현재 시간
                 const d = dayjs.duration(diffDate);
                 const checkminus = d.milliseconds();
+                // 마감 시간 - 현재 시간이 음수 = 이미 종료된 모임
                 if (checkminus <= 0)
                     showUserMyUniveResult[0][i].d_day = "종료";
                 else {
@@ -83,8 +101,10 @@ export const showUserMyUnive = async (user_id) => {
                 }
             }
         }
+        connection.release();
         return showUserMyUniveResult[0];
     } else {
+        connection.release();
         return errResponse(baseResponse.PROFILE_INFO_NOT_EXIST);
     }
 };
@@ -92,27 +112,36 @@ export const showUserMyUnive = async (user_id) => {
 export const showUserParticipate = async (user_id) => {
     const connection = await pool.getConnection(async (conn) => conn);
     const showUserParticipateResult = await selectUserParticipatebyId(connection, user_id);
-    connection.release();
 
     if(showUserParticipateResult[0][0]) {
         for (let i = 0; i < showUserParticipateResult[0].length; i++) {
             //학번 변경
-            if(showUserParticipateResult[0][i].class_of) {
+            if (showUserParticipateResult[0][i].class_of) {
                 const changeClassof = Math.floor(showUserParticipateResult[0][i].class_of / 100000 % 100);
                 showUserParticipateResult[0][i].class_of = changeClassof + "학번"
             }
 
             //모임 시간 변경
-            if(showUserParticipateResult[0][i].meeting_date) {
+            if (showUserParticipateResult[0][i].meeting_date) {
                 const datevalue = dayjs(showUserParticipateResult[0][i].meeting_date);
-                showUserParticipateResult[0][i].meeting_date = datevalue.month()+ 1 + "월" + datevalue.date() + "일";
-                showUserParticipateResult[0][i].meeting_time = datevalue.hour() + ":" + datevalue.minute();
+                showUserParticipateResult[0][i].meeting_date = datevalue.month() + 1 + "월" + datevalue.date() + "일 " + datevalue.hour() + ":" + datevalue.minute();
+            }
 
-                //D-day 추가
-                const nowDate = dayjs();
-                const diffDate = datevalue.diff(nowDate);
+            const postInfo = await postIdCheck(showUserParticipateResult[0][i].post_id);
+
+            // 모임 인원 계산
+            const current_people = postInfo[0][0].current_people;
+            const limit_people = postInfo[0][0].limit_people;
+            showUserParticipateResult[0][i].people_status = current_people + "/" + limit_people;
+
+            //D-day 계산
+            if(postInfo[0][0].end_date) {
+                const nowDate = dayjs(); // 현재 시간
+                const endDate = dayjs(postInfo[0][0].end_date) // 마감 시간
+                const diffDate = endDate.diff(nowDate);  // 마감 시간 - 현재 시간
                 const d = dayjs.duration(diffDate);
                 const checkminus = d.milliseconds();
+                // 마감 시간 - 현재 시간이 음수 = 이미 종료된 모임
                 if (checkminus <= 0)
                     showUserParticipateResult[0][i].d_day = "종료";
                 else {
@@ -123,8 +152,10 @@ export const showUserParticipate = async (user_id) => {
                 }
             }
         }
+        connection.release();
         return showUserParticipateResult[0];
     } else {
+        connection.release();
         return errResponse(baseResponse.PROFILE_INFO_NOT_EXIST);
     }
 };
