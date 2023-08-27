de5e646681e481c414dcccf364cb23921d47601a
JINHYEONG KIM <sgo5178@kyonggi.ac.kr>
2023-08-27 22:58:10 +0900

    Update: 유니버스 참여

--- config/response.js
+++ config/response.js
@@ -34,6 +34,7 @@ export const baseResponse = {
     POST_CONTENT_LENGTH : { "isSuccess": false, "code": 3006, "message": "소개글은 최대 500자리를 입력해주세요." },
     POST_INVITE_EMPTY : { "isSuccess": false, "code": 3007, "message": "아무도 초대하지 않았습니다." },
     POST_PARTICIPATION_CLOSE : { "isSuccess": false, "code": 3008, "message": "모집이 마감되었습니다." },
+    POST_PARTICIPATION_OVERLAP : { "isSuccess": false, "code": 3009, "message": "이미 참여한 유저입니다." },
 
 
 
