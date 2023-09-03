d5b9e65d8c2a3ff2f5d260975a4138fd92251b1b
임채연 <dlacodus0407@gmail.com>
2023-09-04 01:33:26 +0900

    Add: 관리자 페이지 : 유저 정보확인

---
+++ src/app/admin/adminProvider.js
@@ -0,0 +1,18 @@
+import { selectIsAdminByUserEmail, selectUserInfoFromUser } from "./adminDao";
+import pool from "../../../config/database";
+
+/** 관리자인지 확인 함수 */
+export const isAdmin = async(userEmail) => {
+    const connection = await pool.getConnection(async(conn) => conn);
+    const isAdminResult = await selectIsAdminByUserEmail(connection, userEmail);
+    connection.release();
+    if(isAdminResult == 1) return true;
+    return false;
+}
+
+export const getAllUsersInfo = async() => {
+    const connection = await pool.getConnection(async(conn) => conn);
+    const getUsersInfoResult = await selectUserInfoFromUser(connection);
+    connection.release();
+    return getUsersInfoResult;
+}
