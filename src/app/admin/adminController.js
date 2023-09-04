ac1d71e355712abb4828fd28b73d4d4601ee05de
JINHYEONG KIM <sgo5178@kyonggi.ac.kr>
2023-09-05 08:40:46 +0900

    Add: 게시글 작성 (관리자)

--- src/app/admin/adminController.js
+++ src/app/admin/adminController.js
@@ -3,7 +3,7 @@ import { baseResponse, response, errResponse } from "../../../config/response";
 import { getAllUsersInfo } from "./adminProvider";
 import { changeStatusByAdmin, changeHiddenByAdmin } from "./adminService"
 import { retrievePost } from "../post/postProvider";
-import { editPost, removePost } from "../post/postService";
+import { createPost, editPost, removePost } from "../post/postService";
 
 export const getUsersInfo = async(req, res) => {
     const userEmail = req.verifiedToken.userEmail;
@@ -13,6 +13,27 @@ export const getUsersInfo = async(req, res) => {
     const getAllUsersInfoResult = await getAllUsersInfo();
 
     return res.send(response(baseResponse.SUCCESS, getAllUsersInfoResult));
+};
+
+/**
+ * API name : 게시글 작성 (관리자)
+ * POST: admin/post
+ */
+export const postPostByAdmin = async(req, res) => {
+    
+    const {category, limit_gender, limit_people, location, meeting_date, openchat, 
+        end_date, title, content } = req.body;
+    const userEmail = req.verifiedToken.userEmail;
+    const userIdFromJWT = await getUserIdByEmail(userEmail); // 토큰을 통해 얻은 유저 ID (관리자 id) 
+
+    if(!isAdmin(userEmail)){  
+        return res.send(errResponse(baseResponse.NOT_ADMIN));
+    }
+    else{ //접속한 유저가 관리자라면
+        const postPostByAdminResult = await createPost(userIdFromJWT, category, limit_gender, limit_people, location, meeting_date, openchat, 
+             end_date, title, content);       
+         return res.status(200).json(response(baseResponse.SUCCESS, postPostByAdminResult)); 
+    }
 };
 
 /**
--- src/app/admin/adminRoute.js
+++ src/app/admin/adminRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import{ getUsersInfo, patchPostByAdmin, deletePostByAdmin, patchStatusByAdmin, patchHiddenByAdmin } from "./adminController";
+import{ getUsersInfo, postPostByAdmin, patchPostByAdmin, deletePostByAdmin, patchStatusByAdmin, patchHiddenByAdmin } from "./adminController";
 import { jwtMiddleware } from "../../../config/jwtMiddleWare";
 const adminRouter = express.Router();
 
 adminRouter.get('/users-info', jwtMiddleware, getUsersInfo);
+adminRouter.post('/post', jwtMiddleware,postPostByAdmin); // 게시글 작성
 adminRouter.patch('/post/:post_id', jwtMiddleware,patchPostByAdmin); // 게시글 수정 
 adminRouter.delete('/post/:post_id', jwtMiddleware, deletePostByAdmin); // 게시글 삭제 
 adminRouter.patch('/post/:post_id/status', jwtMiddleware, patchStatusByAdmin); // 게시글 상태 변경
