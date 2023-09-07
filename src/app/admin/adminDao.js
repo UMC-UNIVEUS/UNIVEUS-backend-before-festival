25bf6914673eaa2766ef94663591139418a9ea7f
임채연 <dlacodus0407@gmail.com>
2023-09-07 18:52:46 +0900

    Add : 관리자 확인 미들웨어 추가

--- src/app/admin/adminDao.js
+++ src/app/admin/adminDao.js
@@ -3,7 +3,7 @@ export const selectIsAdminByUserEmail = async(connection, userEmail) => {
     const selectIsAdminByUserEmailQuery = `SELECT is_admin FROM user WHERE email_id = '${userEmail}'`;
     const [selectIsAdminByUserEmailRow] = await connection.query(selectIsAdminByUserEmailQuery, userEmail);
 
-    return selectIsAdminByUserEmailRow[0];
+    return selectIsAdminByUserEmailRow[0].is_admin;
 };
 
 export const selectUserInfoFromUser = async(connection) => {
