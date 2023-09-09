b6d6c8f8d438536eff766ab9470b54a27c073443
임채연 <dlacodus0407@gmail.com>
2023-09-09 16:30:33 +0900

    Add : 신고 유저 확인

--- src/app/admin/adminRoute.js
+++ src/app/admin/adminRoute.js
@@ -1,23 +1,29 @@
 import express from "express";
 import{ getUsersInfo, postPostByAdmin, patchPostByAdmin, 
     deletePostByAdmin, patchStatusByAdmin, patchHiddenByAdmin,
-    adminSignUp, userReports, postReports} from "./adminController";
+    adminSignUp, userReports, postReports, adminHome, userHome, 
+    adminSignUpPage} from "./adminController";
 import { jwtMiddleware } from "../../../config/jwtMiddleWare";
 import { adminMiddleware } from "../../../config/adminMiddleware";
 
 const adminRouter = express.Router();
 
-adminRouter.use(jwtMiddleware);
-adminRouter.use(adminMiddleware);
+// adminRouter.use(jwtMiddleware);
+// adminRouter.use(adminMiddleware);
 
-adminRouter.get('/users-info', getUsersInfo);
+adminRouter.get('/', adminHome);
 adminRouter.post('/post',postPostByAdmin); // 게시글 작성
 adminRouter.patch('/post/:post_id',patchPostByAdmin); // 게시글 수정 
 adminRouter.delete('/post/:post_id', deletePostByAdmin); // 게시글 삭제 
 adminRouter.patch('/post/:post_id/status', patchStatusByAdmin); // 게시글 상태 변경
 adminRouter.patch('/post/:post_id/hidden', patchHiddenByAdmin); // 게시글 공개, 비공개 설정
-adminRouter.get('/signup', adminSignUp);
-adminRouter.get('/user-reports', userReports);
+
+adminRouter.post('/user/signup', adminSignUp);
+adminRouter.get('/user/signup', adminSignUpPage)
+adminRouter.get('/user/info', getUsersInfo);
+adminRouter.get('/user/reports', userReports); // 신고 유저 확인
+adminRouter.get('/user', userHome);
 adminRouter.get('/post-reports', postReports);
+
 
 export default adminRouter;
