e04df34120e05fd55e615cd524c8105dc1b7e893
임채연 <dlacodus0407@gmail.com>
2023-09-10 22:37:12 +0900

    Update : 미들웨어로 유저의 계정 상태 확인

--- src/app/report/reportRoute.js
+++ src/app/report/reportRoute.js
@@ -1,9 +1,14 @@
 import express from "express";
 import { reportUser, reportPost } from "./reportController";
 import { jwtMiddleware } from "../../../config/jwtMiddleWare";
+import { accountStatusMiddleware } from "../../../config/accountStatusMiddleware";
+
 const reportRouter = express.Router();
 
-reportRouter.post("/user", jwtMiddleware, reportUser);
-reportRouter.post("/post", jwtMiddleware, reportPost);
+reportRouter.use(jwtMiddleware);
+reportRouter.use(accountStatusMiddleware);
+
+reportRouter.post("/user", reportUser);
+reportRouter.post("/post", reportPost);
 
 export default reportRouter;
