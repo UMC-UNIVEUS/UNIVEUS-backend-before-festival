bd9bca3e7219b20db655695827aeaa70ad8e93d8
임채연 <dlacodus0407@gmail.com>
2023-09-10 22:13:35 +0900

    Update : 미들웨어로 유저의 계정 상태 확인

--- src/app/main/mainRoute.js
+++ src/app/main/mainRoute.js
@@ -1,10 +1,11 @@
 import express from "express"
 import { getPostListPage, searchTitle } from "./mainController"
 import { jwtMiddleware } from "../../../config/jwtMiddleWare";
+import { accountStatusMiddleware } from "../../../config/accountStatusMiddleware";
 
 const mainRouter = express.Router();
 
-mainRouter.get('/', jwtMiddleware, getPostListPage);
+mainRouter.get('/', jwtMiddleware, accountStatusMiddleware, getPostListPage);
 mainRouter.get('/search', searchTitle);
 
 export default mainRouter;
