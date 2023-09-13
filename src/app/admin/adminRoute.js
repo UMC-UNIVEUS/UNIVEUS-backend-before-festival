c299721d6f56b366b94a830eba8f51ae414ac588
ReadMyMemory <lame3925@gmail.com>
2023-09-13 21:00:48 +0900

    Add : 에러 핸들러 추가

--- src/app/admin/adminRoute.js
+++ src/app/admin/adminRoute.js
@@ -7,32 +7,34 @@ import{ getUsersInfo, postPostByAdmin, patchPostByAdmin,
     adminLogin} from "./adminController";
 import { jwtMiddleware } from "../../../config/jwtMiddleWare";
 import { adminMiddleware } from "../../../config/adminMiddleware";
+import {wrapAsync} from "../../../config/errorhandler";
+
 
 const adminRouter = express.Router();
 
-adminRouter.use(jwtMiddleware);
-adminRouter.use(adminMiddleware);
+adminRouter.use(wrapAsync(jwtMiddleware));
+adminRouter.use(wrapAsync(adminMiddleware));
 
 // adminRouter.get('/', adminHomePage);
-adminRouter.post('/post',postPostByAdmin); // 게시글 작성
-adminRouter.patch('/post/:post_id',patchPostByAdmin); // 게시글 수정 
-adminRouter.delete('/post/:post_id', deletePostByAdmin); // 게시글 삭제 
-adminRouter.patch('/post/:post_id/status', patchStatusByAdmin); // 게시글 상태 변경
-adminRouter.patch('/post/:post_id/hidden', patchHiddenByAdmin); // 게시글 공개, 비공개 설정
+adminRouter.post('/post', wrapAsync(postPostByAdmin)); // 게시글 작성
+adminRouter.patch('/post/:post_id', wrapAsync(patchPostByAdmin)); // 게시글 수정
+adminRouter.delete('/post/:post_id', wrapAsync(deletePostByAdmin)); // 게시글 삭제
+adminRouter.patch('/post/:post_id/status', wrapAsync(patchStatusByAdmin)); // 게시글 상태 변경
+adminRouter.patch('/post/:post_id/hidden', wrapAsync(patchHiddenByAdmin)); // 게시글 공개, 비공개 설정
 
-adminRouter.post('/user/signup', adminSignUp);
+adminRouter.post('/user/signup', wrapAsync(adminSignUp));
 // adminRouter.get('/user/signup', adminSignUpPage)
 
-adminRouter.get('/user/info', getUsersInfo);
-adminRouter.get('/user/reports', userReports); // 신고 유저 확인
+adminRouter.get('/user/info', wrapAsync(getUsersInfo));
+adminRouter.get('/user/reports', wrapAsync(userReports)); // 신고 유저 확인
 // adminRouter.get('/user', userHomePage); 
-adminRouter.get('/post-reports', postReports);
+adminRouter.get('/post-reports', wrapAsync(postReports));
 
 // adminRouter.get('/user/block/:reportId/:userId', adminUserBlockPage);
-adminRouter.put('/user/block/:reportId/:userId', adminUserBlock); //유저 제재
-adminRouter.patch('/user/block/free/:reportedId/:userId', adminUserBlockFree); // 제재 해제
+adminRouter.put('/user/block/:reportId/:userId', wrapAsync(adminUserBlock)); //유저 제재
+adminRouter.patch('/user/block/free/:reportedId/:userId', wrapAsync(adminUserBlockFree)); // 제재 해제
 
 // adminRouter.get('/login', adminLoginPage);
-adminRouter.post('/login', adminLogin);
+adminRouter.post('/login', wrapAsync(adminLogin));
 
 export default adminRouter;
--- src/app/report/reportRoute.js
+++ src/app/report/reportRoute.js
@@ -2,13 +2,14 @@ import express from "express";
 import { reportUser, reportPost } from "./reportController";
 import { jwtMiddleware } from "../../../config/jwtMiddleWare";
 import { accountStatusMiddleware } from "../../../config/accountStatusMiddleware";
+import {wrapAsync} from "../../../config/errorhandler";
 
 const reportRouter = express.Router();
 
 reportRouter.use(jwtMiddleware);
 // reportRouter.use(accountStatusMiddleware);
 
-reportRouter.post("/user", reportUser);
-reportRouter.post("/post", reportPost);
+reportRouter.post("/user", wrapAsync(reportUser));
+reportRouter.post("/post", wrapAsync(reportPost));
 
 export default reportRouter;
