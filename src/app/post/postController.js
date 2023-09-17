be0a4ae83eae9f92ec1141901b820de0c836910a
JINHYEONG KIM <sgo5178@kyonggi.ac.kr>
2023-09-17 23:57:20 +0900

    Update: 알림 문자 주석 해체

--- src/app/post/postController.js
+++ src/app/post/postController.js
@@ -504,8 +504,8 @@ export const participateUniveus = async(req, res) => {
 
         await changeParticipateAvailable(userIdFromJWT);
 
-        // const MessageAlarmList = [Writer, [alreadyParticipant], Invitee, [guest]];
-        // await sendParticipantMessageAlarm(post_id, MessageAlarmList); //게시글 참여 시 문자 알림 (to old 참여자, new 참여자)
+        const MessageAlarmList = [Writer, [alreadyParticipant], Invitee, [guest]];
+        await sendParticipantMessageAlarm(post_id, MessageAlarmList); //게시글 참여 시 문자 알림 (to old 참여자, new 참여자)
         return res.send(response(baseResponse.SUCCESS));          
     }  
 
@@ -576,8 +576,8 @@ export const participateUniveus = async(req, res) => {
 
         await changeParticipateAvailable(userIdFromJWT);
 
-        // const MessageAlarmList = [Writer, [alreadyParticipant1, alreadyParticipant2], Invitee, [guest1, guest2]];
-        // await sendParticipantMessageAlarm(post_id, MessageAlarmList); //게시글 참여 시 문자 알림 (to old 참여자, new 참여자)
+        const MessageAlarmList = [Writer, [alreadyParticipant1, alreadyParticipant2], Invitee, [guest1, guest2]];
+        await sendParticipantMessageAlarm(post_id, MessageAlarmList); //게시글 참여 시 문자 알림 (to old 참여자, new 참여자)
 
         return res.send(response(baseResponse.SUCCESS));
         }  
