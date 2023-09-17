2e4f29bbc7dec6b89264aa24f7c4d9868cf6eebe
JaeHyun Lee <122192096+ReadMyMemory@users.noreply.github.com>
2023-09-18 01:54:45 +0900

    Update : 메시지 문구 수정

--- src/app/user/userController.js
+++ src/app/user/userController.js
@@ -140,8 +140,8 @@ export const sendCreatePostMessageAlarm = async(user_id, post_id,participants) =
 
 *매칭 시 유의사항*
 
-- 유니버스는 우리학교 학우들이 모인 공간입니다. 부적절 한 행위 적발 시 서비스 이용에 제약이 있을 수 있습니다. 학우들과 즐거운 추억을 만들어 보아요 :)
-- 유니버스는 반 익명성을 지향하고 있습니다. 신뢰성있는 유익한 소셜링을 진행해 주시길 부탁드립니다.
+- 유니버스는 우리 학교 학우들이 모인 공간입니다. 부적절한 행위 적발 시 서비스 이용에 제약이 있을 수 있습니다. 학우들과 즐거운 추억을 만들어 보아요 :)
+- 유니버스는 반 익명성을 지향하고 있습니다. 신뢰성 있는 유익한 소셜링을 진행해 주시길 부탁드립니다.
 - 긴급상황 발생 시 [https://www.instagram.com/unive.us] 로 연락주시면 운영자가 달려가 조치하겠습니다.`; 
    const { success1 } = await sendSMS(naverCloudSensSecret, { to: writerPhone, content });
    const { success2 } = await sendSMS(naverCloudSensSecret, { to: participantPhone, content });
@@ -173,8 +173,8 @@ export const sendCreatePostMessageAlarm = async(user_id, post_id,participants) =
 
 *매칭 시 유의사항*
 
-- 유니버스는 우리학교 학우들이 모인 공간입니다. 부적절 한 행위 적발 시 서비스 이용에 제약이 있을 수 있습니다. 학우들과 즐거운 추억을 만들어 보아요 :)
-- 유니버스는 반 익명성을 지향하고 있습니다. 신뢰성있는 유익한 소셜링을 진행해 주시길 부탁드립니다.
+- 유니버스는 우리 학교 학우들이 모인 공간입니다. 부적절한 행위 적발 시 서비스 이용에 제약이 있을 수 있습니다. 학우들과 즐거운 추억을 만들어 보아요 :)
+- 유니버스는 반 익명성을 지향하고 있습니다. 신뢰성 있는 유익한 소셜링을 진행해 주시길 부탁드립니다.
 - 긴급상황 발생 시 [https://www.instagram.com/unive.us] 로 연락주시면 운영자가 달려가 조치하겠습니다.`;
     const { success1 } = await sendSMS(naverCloudSensSecret, { to: writerPhone, content });
     const { success2 } = await sendSMS(naverCloudSensSecret, { to: participant1Phone, content });
@@ -211,12 +211,12 @@ export const sendParticipantMessageAlarm = async(post_id, MessageAlarmList) =>{ 
 - 나의 유니버스 오픈채팅방 : ${Post.openchat}
     
 *오픈채팅방에 아직 입장하지 않으셨다면 꼭 입장해서 소통해 주세요 :)
-*모임시간에 늦지 않게 시간과 장소를 잘 확인해 주세요!
+*모임 시간에 늦지 않게 시간과 장소를 잘 확인해 주세요!
 
 *매칭 시 유의사항*
 
-- 유니버스는 우리학교 학우들이 모인 공간입니다. 부적절 한 행위 적발 시 서비스 이용에 제약이 있을 수 있습니다. 학우들과 즐거운 추억을 만들어 보아요 :)
-- 유니버스는 반 익명성을 지향하고 있습니다. 신뢰성있는 유익한 소셜링을 진행해 주시길 부탁드립니다.
+- 유니버스는 우리 학교 학우들이 모인 공간입니다. 부적절한 행위 적발 시 서비스 이용에 제약이 있을 수 있습니다. 학우들과 즐거운 추억을 만들어 보아요 :)
+- 유니버스는 반 익명성을 지향하고 있습니다. 신뢰성 있는 유익한 소셜링을 진행해 주시길 부탁드립니다.
 - 긴급상황 발생 시 [https://www.instagram.com/unive.us] 로 연락주시면 운영자가 달려가 조치하겠습니다.
 
 즐거우셨다면 유니버스를 평가해 주세요! 큰 도움이 됩니다!!
@@ -248,12 +248,12 @@ export const sendParticipantMessageAlarm = async(post_id, MessageAlarmList) =>{ 
 - 나의 유니버스 오픈채팅방 : ${Post.openchat}
     
 *오픈채팅방에 아직 입장하지 않으셨다면 꼭 입장해서 소통해 주세요 :)
-*모임시간에 늦지 않게 시간과 장소를 잘 확인해 주세요!
+*모임 시간에 늦지 않게 시간과 장소를 잘 확인해 주세요!
 
 *매칭 시 유의사항*
 
-- 유니버스는 우리학교 학우들이 모인 공간입니다. 부적절 한 행위 적발 시 서비스 이용에 제약이 있을 수 있습니다. 학우들과 즐거운 추억을 만들어 보아요 :)
-- 유니버스는 반 익명성을 지향하고 있습니다. 신뢰성있는 유익한 소셜링을 진행해 주시길 부탁드립니다.
+- 유니버스는 우리 학교 학우들이 모인 공간입니다. 부적절한 행위 적발 시 서비스 이용에 제약이 있을 수 있습니다. 학우들과 즐거운 추억을 만들어 보아요 :)
+- 유니버스는 반 익명성을 지향하고 있습니다. 신뢰성 있는 유익한 소셜링을 진행해 주시길 부탁드립니다.
 - 긴급상황 발생 시 [https://www.instagram.com/unive.us] 로 연락주시면 운영자가 달려가 조치하겠습니다.
 
 즐거우셨다면 유니버스를 평가해 주세요! 큰 도움이 됩니다!!
